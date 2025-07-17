b0e95b134c344eeeea671c0147e1e6456b8ad9ab
Naresh Narnapati <naresh.narnapati@reymould.com>
2025-07-17 19:06:16 +0530

    All pages and Header navigation completed

--- assets/JS-animations/JS-animations/Header.js
+++ assets/JS-animations/JS-animations/Header.js
@@ -5,3 +5,17 @@ navToggle.addEventListener('click', () => {
     navToggle.classList.toggle('open');
     mainNav.classList.toggle('show');
 });
+
+
+// Header highlighitng by navigation
+document.addEventListener("DOMContentLoaded", function () {
+    const currentPath = window.location.pathname.split("/").pop(); // e.g., 'about-us.html'
+    const navLinks = document.querySelectorAll("nav a");
+
+    navLinks.forEach(link => {
+        const linkPath = link.getAttribute("href").split("/").pop();
+        if (linkPath === currentPath) {
+            link.classList.add("active");
+        }
+    });
+});
